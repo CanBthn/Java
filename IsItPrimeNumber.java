6ff1defa8cb2ad9ca31463fcc8797637ec63ad79
CnBatuhan <138293421+CnBatuhan@users.noreply.github.com>
2023-07-17 16:08:59 +0300

    Add files via upload

---
+++ IsItPrimeNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class IsItPrimeNumber {
+
+    public static boolean prime(int a){
+        for(int i=2; i<a; i++){
+            if(a%i == 0){
+                return false;
+            }
+        }return true;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER VALUE");
+        int sayi = sc.nextInt();
+        for(int i = 0; i<sayi; i++){
+            if(prime(i)){
+                if(i == 0 || i == 1){
+                    continue;
+                }
+                System.out.println(i);
+        }
+
+        }
+    }
+}
